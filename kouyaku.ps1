30647d25482471bfd9d994a0f0f4b6db0796739b
hesonoo <hesonoo1234@gmail.com>
2026-08-05 21:25:14 +0900

    Update kouyaku.ps1

--- kouyaku.ps1
+++ kouyaku.ps1
@@ -1,10 +1,10 @@
-@'
 $pref = Read-Host "Enter prefecture name"
 $count = [int](Read-Host "Enter number of election districts")
 
-for ($i = 0; $i -le $count; $i++) {
+for ($i = 1; $i -le $count; $i++) {
     New-Item -ItemType File -Name "$pref$i.html" -Force
 }
 
-Write-Host "$pref : created $($count + 1) files."
-'@ | Set-Content -Path .\kouyaku.ps1 -Encoding UTF8
+git add .
+git commit -m "Generate $pref HTML files"
+git push
